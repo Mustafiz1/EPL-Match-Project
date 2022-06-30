7d2a548f67104cfb25edcb50be475da3f55947e6
Md. Mustafizur Rahman <107886982+Mustafiz1@users.noreply.github.com>
2022-06-30 18:24:14 +0600

    Add files via upload

---
+++ EPL project.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+pd.read_csv('Premier League Player Stats.csv')
+
+
+# In[4]:
+
+
+import pip
+pip.main(['install', 'scikit-optimize==0.8.1'])
+
+
+# In[5]:
+
+
+import seaborn as sns
+
+
+# In[ ]:
+
+
+
+
